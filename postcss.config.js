7a7f432394dbfc3762b7ce4eb48dea3bd4f9fb93
Steve Sowerby <steve.sowerby@gmail.com>
2024-03-10 18:06:24 +0000

    Add cssnano to prod build

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
 let tailwindConfig = process.env.HUGO_FILE_TAILWIND_CONFIG_JS || './tailwind.config.js';
 const tailwind = require('tailwindcss')(tailwindConfig);
 const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 module.exports = {
     // eslint-disable-next-line no-process-env
-    plugins: [tailwind, ...(process.env.HUGO_ENVIRONMENT === 'production' ? [autoprefixer] : [])],
+    plugins: [
+        tailwind,
+        ...(process.env.HUGO_ENVIRONMENT === 'production' ? [autoprefixer,cssnano] : [])
+    ],
 };
